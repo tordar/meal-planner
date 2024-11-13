fe4304078c308f7129dd45c062bcbb97606aac7a
Tordar Tømmervik <tordar.tommervik@gmail.com>
2024-11-13 15:19:56 +0100

    removed overflow inside rows

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -45,90 +45,88 @@ export function DataTable<T extends { _id: string }>({
     };
 
     return (
-        <div className="relative">
-            <div className="overflow-x-auto">
-                <Table>
-                    <TableHeader>
-                        <TableRow>
-                            {columns.map((column) => (
-                                <TableHead
-                                    key={column.key as string}
-                                    className={column.hideOnMobile ? 'hidden md:table-cell' : ''}
-                                    style={{ width: column.width }}
-                                >
-                                    {column.header}
-                                </TableHead>
-                            ))}
-                            <TableHead className="w-20 md:w-24">Actions</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {data.map((item) => (
-                            <React.Fragment key={item._id}>
-                                <TableRow>
-                                    {columns.map((column) => (
-                                        <TableCell
-                                            key={`${item._id}-${column.key as string}`}
-                                            className={`${
-                                                column.hideOnMobile ? 'hidden md:table-cell' : ''
-                                            } ${column.key === 'name' ? 'pr-20' : ''}`}
+        <div className="w-full">
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        {columns.map((column) => (
+                            <TableHead
+                                key={column.key as string}
+                                className={column.hideOnMobile ? 'hidden md:table-cell' : ''}
+                                style={{ width: column.width }}
+                            >
+                                {column.header}
+                            </TableHead>
+                        ))}
+                        <TableHead className="w-20 md:w-24">Actions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {data.map((item) => (
+                        <React.Fragment key={item._id}>
+                            <TableRow>
+                                {columns.map((column) => (
+                                    <TableCell
+                                        key={`${item._id}-${column.key as string}`}
+                                        className={`${
+                                            column.hideOnMobile ? 'hidden md:table-cell' : ''
+                                        } ${column.key === 'name' ? 'pr-20' : ''}`}
+                                    >
+                                        <div className={column.key === 'name' ? 'font-medium' : ''}>
+                                            {renderCellContent(item, column)}
+                                        </div>
+                                    </TableCell>
+                                ))}
+                                <TableCell className="w-20 md:w-24">
+                                    <div className="flex items-center justify-end space-x-2">
+                                        <Button
+                                            variant="ghost"
+                                            size="icon"
+                                            onClick={() => toggleRowExpansion(item._id)}
+                                            className="md:hidden"
                                         >
-                                            <div className={column.key === 'name' ? 'font-medium' : ''}>
-                                                {renderCellContent(item, column)}
-                                            </div>
-                                        </TableCell>
-                                    ))}
-                                    <TableCell className="w-20 md:w-24">
-                                        <div className="flex items-center justify-end space-x-2">
-                                            <Button
-                                                variant="ghost"
-                                                size="icon"
-                                                onClick={() => toggleRowExpansion(item._id)}
-                                                className="md:hidden"
-                                            >
-                                                {expandedRows.has(item._id) ? (
-                                                    <ChevronUp className="h-4 w-4" />
-                                                ) : (
-                                                    <ChevronDown className="h-4 w-4" />
-                                                )}
-                                            </Button>
-                                            <DropdownMenu>
-                                                <DropdownMenuTrigger asChild>
-                                                    <Button variant="ghost" size="icon">
-                                                        <MoreHorizontal className="h-4 w-4" />
-                                                        <span className="sr-only">Open menu</span>
-                                                    </Button>
-                                                </DropdownMenuTrigger>
-                                                <DropdownMenuContent align="end">
-                                                    <DropdownMenuItem onClick={() => onEdit(item)}>
-                                                        Edit
-                                                    </DropdownMenuItem>
-                                                    <DropdownMenuItem onClick={() => onDelete(item._id)}>
-                                                        Delete
-                                                    </DropdownMenuItem>
-                                                </DropdownMenuContent>
-                                            </DropdownMenu>
+                                            {expandedRows.has(item._id) ? (
+                                                <ChevronUp className="h-4 w-4" />
+                                            ) : (
+                                                <ChevronDown className="h-4 w-4" />
+                                            )}
+                                        </Button>
+                                        <DropdownMenu>
+                                            <DropdownMenuTrigger asChild>
+                                                <Button variant="ghost" size="icon">
+                                                    <MoreHorizontal className="h-4 w-4" />
+                                                    <span className="sr-only">Open menu</span>
+                                                </Button>
+                                            </DropdownMenuTrigger>
+                                            <DropdownMenuContent align="end">
+                                                <DropdownMenuItem onClick={() => onEdit(item)}>
+                                                    Edit
+                                                </DropdownMenuItem>
+                                                <DropdownMenuItem onClick={() => onDelete(item._id)}>
+                                                    Delete
+                                                </DropdownMenuItem>
+                                            </DropdownMenuContent>
+                                        </DropdownMenu>
+                                    </div>
+                                </TableCell>
+                            </TableRow>
+                            {expandedRows.has(item._id) && (
+                                <TableRow className="md:hidden">
+                                    <TableCell colSpan={columns.length + 1}>
+                                        <div className="py-2 space-y-2">
+                                            {columns.filter(column => column.hideOnMobile).map((column) => (
+                                                <div key={column.key as string}>
+                                                    <strong>{column.header}:</strong> {renderCellContent(item, column)}
+                                                </div>
+                                            ))}
                                         </div>
                                     </TableCell>
                                 </TableRow>
-                                {expandedRows.has(item._id) && (
-                                    <TableRow className="md:hidden">
-                                        <TableCell colSpan={columns.length + 1}>
-                                            <div className="py-2 space-y-2">
-                                                {columns.filter(column => column.hideOnMobile).map((column) => (
-                                                    <div key={column.key as string}>
-                                                        <strong>{column.header}:</strong> {renderCellContent(item, column)}
-                                                    </div>
-                                                ))}
-                                            </div>
-                                        </TableCell>
-                                    </TableRow>
-                                )}
-                            </React.Fragment>
-                        ))}
-                    </TableBody>
-                </Table>
-            </div>
+                            )}
+                        </React.Fragment>
+                    ))}
+                </TableBody>
+            </Table>
         </div>
     )
 }
